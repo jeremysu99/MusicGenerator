56ce0726fa51263da8b1acf44a89cb5f53d0a811
Jeremy Su <jeremysu99@gmail.com>
2024-09-04 21:05:30 -0700

    fd

---
+++ dataset.py
@@ -0,0 +1,12 @@
+from torch.utils.data import Dataset
+
+class UrbanSoundDataset(Dataset):
+    
+    def __init__(self, annotationsFile, audioDir):
+        pass
+    
+    def __len__(self):
+        pass
+    
+    def __getitem__(self, index):
+        pass
